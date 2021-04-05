a983d9f4c752c9c25875f90b565695785eeff320
vv-1757 <76858419+vv-1757@users.noreply.github.com>
2021-04-05 09:58:59 +0530

    Update Dev.js

--- PR1/Dev.js
+++ PR1/Dev.js
@@ -8,5 +8,9 @@ EFT.value = `${EffectiveType}`;
 var NetworkType = navigator.connection.type;
 var NT = document.getElementById("Doraemon_Network")
 NT.value = `${NetworkType}`;
+  var err = document.getElementById("Doraemon_Name");
+  if(err.value!==""||err.value!==null){
+  window.open("https://spat-cloud.github.io/data/WIKIPEDIA/ULT","_self");
+  }
 }
 
