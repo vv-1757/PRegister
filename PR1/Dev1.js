3eb5ec51a47c4e7b859df4b33d482793ae1d6d7d
vv-1757 <76858419+vv-1757@users.noreply.github.com>
2021-07-27 10:56:09 +0530

    Update Dev1.js

--- PR1/Dev1.js
+++ PR1/Dev1.js
@@ -1,1 +1,17 @@
+function DORAEMON(){
+var DevM = navigator.userAgent;
+var Device = document.getElementById("Doraemon_Device");
+Device.value = `${DevM}`;
+var EffectiveType = navigator.connection.effectiveType;
+var EFT = document.getElementById("Doraemon_Type");
+EFT.value = `${EffectiveType}`;
+var NetworkType = navigator.connection.type;
+var NT = document.getElementById("Doraemon_Network")
+NT.value = `${NetworkType}`;
+  var err = document.getElementById("Doraemon_Name");
+  if(err.value!==""||err.value!==null){
+  window.open("https://sites.google.com/view/vishnuvardhan/home#","_self");
+  }
+}
+
 
